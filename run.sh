e744189dd74116f82fabe07be068328b0724446b
Xiaocheng stapc430 <xcchow@126.com>
2025-01-07 13:29:38 +0800

    a minor modification

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ $python_path $proj_path/main.py \
 --model "distiluse-base-multilingual-cased-v1" \
 --tag "result_20250104-2223" \
 
+# zip -r result_20250104-2223.zip result_20250104-2223
+
 
 # 20250103-1510
 $python_path $proj_path/main.py \
@@ -22,3 +24,6 @@ $python_path $proj_path/main.py \
 --mixed_soft \
 --tag "result_20250105-0044" \
 --model "GanymedeNil/text2vec-large-chinese" \
+
+
+# zip -r result_20250105-0044.zip result_20250105-0044
